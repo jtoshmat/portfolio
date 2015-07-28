053f9abd96bfe9e61cceeb87bb7b2328003a4dde
Jon Jandoc <jon@yupso.com>
2015-07-27 17:36:14 -0700

    Move public files to src directory, add gulp tasks for copying them over

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var gulp = require('gulp'),
 gulp.task('default', ['build', 'watch']);
 
 // Build Tasks
-gulp.task('build',  ['lint', 'build-css', 'build-js', 'copy-html']);
+gulp.task('build',  ['lint', 'build-css', 'build-js', 'copy-html', 'copy-images', 'copy-static']);
 
 // lint: Ensure JavaScript is free of errors
 gulp.task('lint', function() {
@@ -27,7 +27,7 @@ gulp.task('build-css', function() {
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('public/css'));
 });
 
 // build-js: minify and concat js, copy to dist
@@ -38,11 +38,21 @@ gulp.task('build-js', function() {
     //only uglify if gulp is ran with '--type production'
     .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest('public/js'));
 });
 
+// copy tasks
 gulp.task('copy-html', function() {
   gulp.src('src/*.html').pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy-images', function() {
+  gulp.src('src/img/*').pipe(gulp.dest('public/img'));
+});
+
+gulp.task('copy-static', function() {
+  return gulp.src(['src/static/**/*', 'src/static/**/.*'])
+    .pipe(gulp.dest('public'));
 });
 
 // Watch files for changes, on change, call compile tasks
@@ -50,11 +60,14 @@ gulp.task('watch', function() {
   gulp.watch('src/js/**/*.js', ['lint', 'build-js']);
   gulp.watch('src/scss/**/*.scss', ['build-css']);
   gulp.watch('src/*.html', ['copy-html']);
+  gulp.watch('src/img/*', ['copy-images']);
+  gulp.watch('src/static/**/*', ['copy-static']);
+  gulp.watch('src/static/**/.*', ['copy-static']);
 });
 
-// Task to clean the dist folder
+// Task to clean the public folder
 gulp.task('clean', function() {
   del([
-  'dist/**/*'
+  'public/**/*'
   ]);
 });
