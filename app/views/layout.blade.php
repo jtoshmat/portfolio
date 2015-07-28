caacadee05dc00f279a472f1f85d7d9c11370ef5
Jon Jandoc <jon@yupso.com>
2015-07-28 10:00:23 -0700

    Moving static resources

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -2,12 +2,7 @@
 <html lang=”en”>
   <head>
     <meta charset="UTF-8" />
-    <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="/css/style.css" />
-    <link rel="stylesheet" href="/css/jquery.dataTables.min.css" />
-    <script src='/js/jquery-git2.min.js'></script>
-    <script src='/js/jquery.dataTables.min.js'></script>
-    <script src='/js/main.js'></script>
+    <link rel="stylesheet" href="/css/main.css" />
     <title>Packers Everywhere</title>
   </head>
   <body>
@@ -25,5 +20,9 @@
       </div>
     </div>
     @include("footer")
+    <script src='/js/vendor/jquery-1.11.3.min.js'></script>
+    <script src='/js/vendor/jquery.dataTables.min.js'></script>
+    <script src='/js/vendor/bootstrap.min.js'></script>
+    <script src='/js/main.js'></script>
   </body>
 </html>
