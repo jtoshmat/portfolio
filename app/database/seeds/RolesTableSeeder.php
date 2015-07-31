a4473704d6da2d315eb10939a40b5ea46d4e323d
Jon Jandoc <jjandoc@bluestatedigital.com>
2015-07-31 11:13:01 -0700

    Fix merge conflict.

--- app/database/seeds/RolesTableSeeder.php
+++ app/database/seeds/RolesTableSeeder.php
@@ -8,19 +8,12 @@ public function run()
     DB::table('roles')->delete();
 
 
-		Role::create(array(
+    Role::create(array(
         'uid' => 1,
-<<<<<<< HEAD
-		    'pusertype' => 1,
-        'privileges' => 1
+  	    'pusertype' => 1,
+        'privileges' => 7
     ));
 
-=======
-	    'pusertype' => 1,
-        'privileges' => 7
-    ));    
-    
->>>>>>> master
 
 
 }
