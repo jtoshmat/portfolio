d91c72c34631ab7eff78f3d2c89f19b437e25745
Jon Jandoc <jjandoc@bluestatedigital.com>
2015-07-31 16:41:45 -0700

    Update layout for addbar view

--- app/views/bars/addbar.blade.php
+++ app/views/bars/addbar.blade.php
@@ -1,69 +1,129 @@
 @extends("layout")
-
 @section("content")
- 
-<br />
+<div class="container">
+  <div class="row">
+    <div class="col-sm-6">
+      <div class="panel panel-default">
+        <div class="panel-heading"><h2 class="panel-title">Add a Bar</h2></div>
+        <div class="panel-body">
+        {{ Form::open(array("url" => "addbar", "class" => "form-signup")) }}
+          <div class="form-group">
+            {{ Form::label("barname", "Bar Name") }}
+            {{ Form::text("barname", Input::old("barname"), ["class" => "form-control", "required"]) }}
+          </div>
+          <div class="form-group">
+            {{ Form::label("address", "Address") }}
+            {{ Form::text("address", Input::old("address"), ["class" => "form-control", "required"]) }}
+          </div>
+          <div class="form-group">
+            {{ Form::label("city", "City") }}
+            {{ Form::text("city", Input::old("city"), ["class" => "form-control", "required"]) }}
+          </div>
+          <div class="row">
+            <div class="col-sm-6">
+              <div class="form-group">
+                {{ Form::label("zipcode", "Zip Code") }}
+                {{ Form::text("zipcode", Input::old("zipcode"), ["class" => "form-control", "required"]) }}
+              </div>
+            </div>
+            <div  class="col-sm-6">
+              <div class="form-group">
+                {{ Form::label("state", "State") }}
+                {{ Form::select('state', array(
+                  "AL" => "Alabama",
+                  "AK" => "Alaska",
+                  "AZ" => "Arizona",
+                  "AR" => "Arkansas",
+                  "CA" => "California",
+                  "CO" => "Colorado",
+                  "CT" => "Connecticut",
+                  "DE" => "Delaware",
+                  "DC" => "District of Columbia",
+                  "FL" => "Florida",
+                  "GA" => "Georgia",
+                  "HI" => "Hawaii",
+                  "ID" => "Idaho",
+                  "IL" => "Illinois",
+                  "IN" => "Indiana",
+                  "IA" => "Iowa",
+                  "KS" => "Kansas",
+                  "KY" => "Kentucky",
+                  "LA" => "Louisiana",
+                  "ME" => "Maine",
+                  "MD" => "Maryland",
+                  "MA" => "Massachusetts",
+                  "MI" => "Michigan",
+                  "MN" => "Minnesota",
+                  "MS" => "Mississippi",
+                  "MO" => "Missouri",
+                  "MT" => "Montana",
+                  "NE" => "Nebraska",
+                  "NV" => "Nevada",
+                  "NH" => "New Hampshire",
+                  "NJ" => "New Jersey",
+                  "NM" => "New Mexico",
+                  "NY" => "New York",
+                  "NC" => "North Carolina",
+                  "ND" => "North Dakota",
+                  "OH" => "Ohio",
+                  "OK" => "Oklahoma",
+                  "OR" => "Oregon",
+                  "PA" => "Pennsylvania",
+                  "RI" => "Rhode Island",
+                  "SC" => "South Carolina",
+                  "SD" => "South Dakota",
+                  "TN" => "Tennessee",
+                  "TX" => "Texas",
+                  "UT" => "Utah",
+                  "VT" => "Vermont",
+                  "VA" => "Virginia",
+                  "WA" => "Washington",
+                  "WV" => "West Virginia",
+                  "WI" => "Wisconsin",
+                  "WY" => "Wyoming",
+                ), Input::old("state"), ["class" => "form-control", "required"]) }}
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-6">
+              <div class="form-group">
+                {{ Form::label("phone", "Phone") }}
+                {{ Form::text("phone", Input::old("phone"), ["class" => "form-control", "required"]) }}
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="form-group">
+                {{ Form::label("url", "Website") }}
+                {{ Form::text("url", Input::old("url"), ["class" => "form-control", "placeholder" => "optional"]) }}
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <button class="btn btn-default" type="button" disabled="">Upload Logo</button>
+          </div>
+          <div class="form-group">
+            {{ Form::label("description", "Promo/Description") }}
+            {{ Form::textarea("description", Input::old("description"), ["class" => "form-control", "placeholder" => "optional"]) }}
+          </div>
 
+          @if (count($errors) > 0)
+          <div class="alert alert-danger" role="alert">
+          @foreach($errors->all() as $error)
+            <p>
+              <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+              <span class="sr-only">Error:</span>
+              {{ $error }}
+            </p>
+          @endforeach
+          </div>
+          @endif
 
-<ul>
-	@foreach($errors->all() as $error)
-		<li>{{ $error }}</li>
-	@endforeach
-</ul>
-
-
-<div  class="table-responsive">
-  {{ Form::open(array('url' => 'addbar')) }}
-  <table class="table table-hover display nowrap dataTable dtr-inline">
-  <tbody>
-
-  <tr>
-  	<TR> <TH COLSPAN=2>Editing</TH> </TR>
-  </tr>
-  
-
-
-  <tr>
- 	<td>Bar Name: </td>
-  <td>{{Form::text('barname')}}</td>
-  </tr>
-
-  <tr>
- 	<td>Address: </td>
-  <td>{{Form::text('address')}}</td>
-  </tr>
-
-  <tr>
- 	<td>City: </td>
-  <td>{{Form::text('city')}}</td>
-  </tr>
-
-
-  <tr>
- 	<td>State: </td>
-  <td>{{Form::text('state')}}</td>
-  </tr>
-
-  <tr>
- 	<td>Zip Code: </td>
-  <td>{{Form::text('zipcode')}}</td>
-  </tr>
-
-
-
-
-  <tr>
-    <TR> <TH COLSPAN=2 style='text-align:center'> 
-     
-      {{Form::reset('Reset')}}
-      {{ Form::submit('Add', ['name' => 'submit']) }}
-        </TH> </TR>
-  </tr>
-
-  </tbody>
-  </table>
-  {{ Form::close() }}
+          {{ Form::submit("Add Bar", ["class" => "btn btn-primary"]) }}
+        {{ Form::close() }}
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 @stop
- 
- 
