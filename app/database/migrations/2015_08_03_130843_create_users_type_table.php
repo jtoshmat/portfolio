0661a5c05ece55c343186b8e79d2139b57e9aacd
bsdloaner <bsdloaner@c02l552mf5v8.local>
2015-08-03 09:29:45 -0400

    user types table created

---
+++ app/database/migrations/2015_08_03_130843_create_users_type_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTypeTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('user_types', function(Blueprint $table)
+		{
+			$table->increments('utid');
+			$table->integer('rid');
+			$table->string("title", 255);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('user_types');
+	}
+
+}
