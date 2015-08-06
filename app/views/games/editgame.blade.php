91016307945de5293dfadf5a8350b3a9051f8cc8
Jon Jandoc <jjandoc@bluestatedigital.com>
2015-08-05 17:23:33 -0700

    Scaffolding for edit game view

--- app/views/games/editgame.blade.php
+++ app/views/games/editgame.blade.php
@@ -1,46 +1,70 @@
 @extends("layout")
-
 @section("content")
-	
-	<h2><a href="{{ URL::route("bars") }}">Bars</a> | <a href="{{ URL::to("games/".$game->bid) }}">Games</a>  | Game</h2>
-	
-	<ul>
-		@foreach($errors->all() as $error)
-			<li>{{ $error }}</li>
-		@endforeach
-	</ul>
+<div class="container add-bar">
+  <div class="page-header">
+    <h2>Season Schedule</h2>
+  </div>
+  <div class="row">
+    <div class="col-sm-8">
+      {{ Form::open(array("url" => "addgame/'.$bid", "class" => "form-add-bar")) }}
+        <div class="form-group">
+          {{ Form::label("datetime", "Date") }}
+          {{Form::hidden("datetime", null, ["class" => "form-control datetime-picker"])}}
+        </div>
+        <div class="form-group">
+          {{ Form::label("vs", "vs") }}
+          <div class="row">
+            <div class="col-xs-8">
+              {{ Form::text("vs", Input::old("vs"), ["class" => "form-control", "required"]) }}
+            </div>
+            <div class="col-xs-4">
+              {{ Form::select("location", array(
+                "home" => "Home",
+                "away" => "Away"
+              ), Input::old("location"), ["class" => "form-control", "required"]) }}
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          {{ Form::label(null, "TV Station") }}
+          <div>
+            <label class="checkbox-inline">{{ Form::checkbox('tv', 'NBC') }} NBC</label>
+            <label class="checkbox-inline">{{ Form::checkbox('tv', 'CBS') }} CBS</label>
+            <label class="checkbox-inline">{{ Form::checkbox('tv', 'ESPN') }} ESPN</label>
+            <label class="checkbox-inline">{{ Form::checkbox('tv', 'FOX') }} FOX</label>
+            <label class="checkbox-inline">{{ Form::checkbox('tv', 'NFL Network') }} NFL Network</label>
+          </div>
+        </div>
+        <div class="form-group">
+          {{ Form::label("notes", "Notes") }}
+          {{ Form::textarea("description", Input::old("description"), ["class" => "form-control", "placeholder" => "optional"]) }}
+        </div>
 
-	<div  class="table-responsive">
-		{{ Form::model($game, array('url' => 'editgame/'.$game->gid)) }}
-		<table class="table table-hover display nowrap dataTable dtr-inline">
-			<tbody>
-
-			<tr>
-			<TR> <TH COLSPAN=2>Editing</TH> </TR>
-			</tr>
-
-
-
-
-			<tr>
-				<td>Game Title: </td>
-				<td>{{Form::text('title')}}</td>
-			</tr>
-
-
-
-
-			<tr>
-			<TR> <TH COLSPAN=2 style='text-align:center'>
-
-					{{Form::reset('Reset')}}
-					{{Form::hidden('gid', $game->gid)}}
-					{{ Form::submit('Update', ['name' => 'submit']) }}
-				</TH> </TR>
-			</tr>
-
-			</tbody>
-		</table>
-		{{ Form::close() }}
-	</div>
+        @if (count($errors) > 0)
+        <div class="alert alert-danger" role="alert">
+        @foreach($errors->all() as $error)
+          <p>
+            <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+            <span class="sr-only">Error:</span>
+            {{ $error }}
+          </p>
+        @endforeach
+        </div>
+        @endif
+        <div class="row">
+          <div class="col-xs-6">
+            <ul id="edit-game-actions" class="list-inline edit-actions">
+              <li>
+                <a href="#" id="delete-game" data-gameid="{{-- Game ID here --}}" class="action-delete-bar"><span class="glyphicon glyphicon-trash" data-toggle="tooltip" data-placement="top" title="Delete" aria-hidden="true"></span><span class="sr-only">Delete Game</span></a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-xs-6 text-right">
+            {{ Form::submit("Add Game", ["class" => "btn btn-primary"]) }}
+          </div>
+        </div>
+      {{ Form::close() }}
+    </div>
+  </div>
+</div>
 @stop
