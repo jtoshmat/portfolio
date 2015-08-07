fddfaf533e919f4fcdab1faf7c925df68c46f73f
Jon Jandoc <jjandoc@bluestatedigital.com>
2015-08-07 14:18:08 -0700

    Update add/edit event views

--- app/views/bars/addbevent.blade.php
+++ app/views/bars/addbevent.blade.php
@@ -1,41 +1,52 @@
 @extends("layout")
-
 @section("content")
+<div class="container add-bar">
+  <div class="page-header">
+    <h2>Edit Event</h2>
+  </div>
+  <div class="row">
+    <div class="col-sm-8">
+      {{ Form::open(array("url" => "addbevent/bid", "class" => "form-add-bar")) }}
+        <div class="form-group">
+          {{ Form::label("datetime", "Date") }}
+          {{ Form::hidden("datetime", null, ["class" => "form-control datetime-picker"]) }}
+        </div>
+        <div class="form-group">
+          {{ Form::label("title", "Title") }}
+          {{Form::text("title", Input::old("title"), ["class" => "form-control", "required"]) }}
+        </div>
+        <div class="form-group">
+          {{ Form::label("description", "Description") }}
+          {{ Form::textarea("description", Input::old("description"), ["class" => "form-control", "placeholder" => "optional"]) }}
+        </div>
 
-	<ul>
-		@foreach($errors->all() as $error)
-			<li>{{ $error }}</li>
-		@endforeach
-	</ul>
-
-<div  class="table-responsive">
-  {{ Form::open(array('url' => 'addbevent/'.$gid)) }}
-  <table class="table table-hover display nowrap dataTable dtr-inline">
-  <tbody>
-
-  <tr>
-  	<TR> <TH COLSPAN=2>Adding a new event</TH> </TR>
-  </tr>
-
-
-  <tr>
- 	<td>Event Title: </td>
-  <td>{{Form::text('title');}}</td>
-  </tr>
-
-
-  <tr>
-    <TR> <TH COLSPAN=2 style='text-align:center'>
-
-		  {{Form::reset('Reset')}}
-		  {{ Form::submit('Add', ['name' => 'submit']) }}
-        </TH> </TR>
-  </tr>
-
-  </tbody>
-  </table>
-  {{ Form::close() }}
+        @if (count($errors) > 0)
+        <div class="alert alert-danger" role="alert">
+        @foreach($errors->all() as $error)
+          <p>
+            <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+            <span class="sr-only">Error:</span>
+            {{ $error }}
+          </p>
+        @endforeach
+        </div>
+        @endif
+        <div class="row">
+          <div class="col-xs-6">
+            <ul id="edit-game-actions" class="list-inline edit-actions">
+              <li>
+                {{-- TODO: hook up event action --}}
+                <a href="#" id="delete-game" data-eventid="{{-- Event ID here --}}" class="action-delete-bar"><span class="glyphicon glyphicon-trash" data-toggle="tooltip" data-placement="top" title="Delete" aria-hidden="true"></span><span class="sr-only">Delete Game</span></a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-xs-6 text-right">
+            {{ Form::submit("Add Event", ["class" => "btn btn-primary"]) }}
+          </div>
+        </div>
+{{--	    {{ Form::hidden('bid', $bid) }}--}}
+      {{ Form::close() }}
+    </div>
+  </div>
 </div>
 @stop
- 
- 
