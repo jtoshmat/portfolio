eed6082f6a596047077cc941b5b0398c8a8028d2
jonschwa <jalexan@gmail.com>
2015-08-11 22:41:39 +0000

    added staging db config, fixed env detection

---
+++ app/config/staging/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'rds-wpdsvc-001.dev.bsd.net',
+            'database'  => 'packers_staging',
+            'username'  => 'packers_staging',
+            'password'  => 'nkJHL8mgaO',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        )
+    )
+);
