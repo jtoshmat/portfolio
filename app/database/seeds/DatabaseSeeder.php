b360d3668e57179abfc10bfbf64ac24a36f5dd91
jonschwa <jalexan@gmail.com>
2015-08-25 17:27:15 +0000

    fixed seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder {
 		$this->call('BarsTableSeeder');
 		$this->call('GamesTableSeeder');
 		$this->call('RefZipDetailsSeeder');
-		$this->call('CreateSessionsTable');
 	}
 
 }
