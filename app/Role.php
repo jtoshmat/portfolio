a12ab6654be383d1496afc30b563ed5cc12bd463
Arron <arronkallenberg@Arrons-MacBook-Pro.local>
2015-10-09 17:02:41 -0400

    using custom config for now to get roles title without calling db

--- app/Role.php
+++ app/Role.php
@@ -8,11 +8,14 @@ class Role extends Model
 {
     protected $table = 'roles';
 
-
     public static function getRole($role_id)
     {
     	//TODO let's not call the DB here.
-    	return self::find($role_id);
+    	//return self::find($role_id);
+
+    	$roles = (array) \Config::get('mycustomvars.roles');
+
+    	return array_search($role_id, $roles);
     }
 
 }
