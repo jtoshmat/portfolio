a92666420ddf51ab271ff889647448da9f96ce1f
Arron <arronkallenberg@Arrons-MacBook-Pro.local>
2015-10-27 16:14:13 -0400

    psring MyMail.php

--- app/cmwn/Services/MyMail.php
+++ app/cmwn/Services/MyMail.php
@@ -1,18 +1,20 @@
 <?php
 
 namespace app\cmwn\Services;
+
 use Illuminate\Support\Facades\Mail;
 
 class MyMail
 {
-	public static function send($mailData){
-		Mail::send($mailData['template'], $mailData, function ($message) use ($mailData) {
-			$message->from('admin@changemyworldnow.education', 'Jon Toshmatov');
-			//$message->cc('jon@ginasink.com', 'Jon at Ginasink');
-			$message->replyTo('admin@changemyworldnow.education', 'toshmatovus@gmail.com');
-			$message->subject($mailData['subject']);
-			$message->priority($mailData['priority']);
-			$message->to($mailData['to']);
-		});
-	}
+    public static function send($mailData)
+    {
+        Mail::send($mailData['template'], $mailData, function ($message) use ($mailData) {
+            $message->from('admin@changemyworldnow.education', 'Jon Toshmatov');
+            //$message->cc('jon@ginasink.com', 'Jon at Ginasink');
+            $message->replyTo('admin@changemyworldnow.education', 'toshmatovus@gmail.com');
+            $message->subject($mailData['subject']);
+            $message->priority($mailData['priority']);
+            $message->to($mailData['to']);
+        });
+    }
 }
