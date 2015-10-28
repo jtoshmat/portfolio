92633181782f9fa7ddf06714a6a8753c52c63362
jontoshmatov <jontoshmatov@yahoo.com>
2015-10-28 13:25:15 -0400

    moved the access controller from index page to VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -11,6 +11,13 @@ class VerifyCsrfToken extends BaseVerifier
      *
      * @var array
      */
+
+    public function __construct(){
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'); // allow certain headers
+    }
+
     protected $except = [
         //
     ];
--- public/index.php
+++ public/index.php
@@ -2,9 +2,9 @@
     /*
      * @TODO: Come up with a better solution for Access Controller
      */
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'); // allow certain headers
+//    header('Access-Control-Allow-Origin: *');
+//    header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+//    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'); // allow certain headers
 
 /**
  * Laravel - A PHP Framework For Web Artisans
