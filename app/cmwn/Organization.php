8828b5b8651ba5e3ceeeb82edc4193c021c06e4c
Arron <arronkallenberg@Arrons-MacBook-Pro.local>
2015-11-10 14:40:13 -0500

    started removing updateGropus

--- app/cmwn/Organization.php
+++ app/cmwn/Organization.php
@@ -49,45 +49,4 @@ class Organization extends Model
 
         return $this->save();
     }
-
-    public static function updateGroups(Request $request)
-    {
-        $titles = $request::get('title');
-        $ids = $request::get('id');
-        $deleteId = $request::get('delete');
-        $newtitle = $request::get('newtitle');
-
-        $i = 0;
-        if ($ids) {
-            foreach ($ids as $id) {
-                $group = self::find($id);
-                $group->title = $titles[ $i ];
-
-                if (isset($deleteId[ $i ]) && $deleteId[ $i ] == $id) {
-                    $group->delete();
-                } else {
-                    $group->save();
-                }
-                ++$i;
-            }
-        }
-        if ($newtitle) {
-            $group = new self();
-            $group->title = $newtitle;
-            $group->save();
-        }
-
-        return true;
-    }
-
-    /*
-     * Scope a query to only include users of a given type.
-     *
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    // public function scopeName($query, $val)
-    // {
-    //     return $query->where('title', $val);
-    // }
 }
-//@TODO needs softdele added
