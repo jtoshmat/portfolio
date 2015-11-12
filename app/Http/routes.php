9cbb415936e7e2c30a2d37a94e605a09c0eaaaec
Arron <arronkallenberg@Arrons-MacBook-Pro.local>
2015-11-12 12:43:26 -0500

    cleaning up controller some more

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,12 +21,13 @@ Route::get('/home', function () {
     return view('welcome');
 });
 
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
 Route::group(['prefix' => 'admin'], function ($router) {
 
-    // Authentication routes...
-    Route::get('/login', 'Auth\AuthController@getLogin');
-    Route::post('/login', 'Auth\AuthController@postLogin');
-    Route::get('/logout', 'Auth\AuthController@getLogout');
+    Route::post('auth/login', 'Auth\AuthController@postLogin');
 
     // Authenticated Users Only
     Route::group(['middleware' => 'auth'], function ($router) {
@@ -57,7 +58,7 @@ Route::group(['prefix' => 'admin'], function ($router) {
 });
 
 ##########################################################################
-############################# API Requests ###############################
+#############################    API Requests ###############################
 ##########################################################################
 
 Route::group(['middleware' => 'api'], function () {
