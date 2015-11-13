dec2e2876105b76b528616e3ae4b628c8c633d95
Arron <arronkallenberg@Arrons-MacBook-Pro.local>
2015-11-13 12:08:10 -0500

    removing from global route, restoring middleware test

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,7 +17,6 @@ class Kernel extends HttpKernel
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \app\Http\Middleware\Api::class,
         \app\Http\Middleware\VerifyCsrfToken::class,
     ];
 
